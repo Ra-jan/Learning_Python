8eed83bed39d06da3bc3342bf3a2e6f10c197f09
Unknownymous <rajankum2468@gmail.com>
2025-09-04 21:21:52 +0530

    Learned something about Tuples

---
+++ Tuples.py
@@ -0,0 +1,22 @@
+# Tuples are very similar to lists but the only difference is it cannot be modified.
+# Tuples are immutable
+# Thw tuple is defined exactly as lists but in ()
+
+# IMMUTABLE
+
+tuple_1 = ('Python', 'Java', 'C++', 'Ruby', 'Go')
+tuple_2 = tuple_1
+print(tuple_1)
+print(tuple_2)
+
+# we cannot make any changes in tuple as we did in the lists, hence it doesn't have a lot of functions/Methods as lists
+''' EXAMPLE:
+If we try to make change in the tuple as we did in list
+tuple_1[0] = 'Art'
+print(tuple_1)
+print(tuple_2)
+
+It will give us error as "Tuple does not support item assignment"
+'''
+
+# We cannot mutate the values such as append,remove etc. but we can access the values, iterate through values, loop through the tuple etc.
